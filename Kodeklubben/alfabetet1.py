6d21fd4e6903b9cfea9ae5296d5a016951bf8506
Cato Kristoffer Myhre <boredgamerz1998@gmail.com>
2017-04-28 15:56:50 +0200

    kidsakoder oppgaver

---
+++ Kodeklubben/alfabetet1.py
@@ -0,0 +1,45 @@
+alphabet = u"abcdefghijklmnopqrstuvwxyzæøå"
+
+def encode(letter, secret):
+    pos = alphabet.find(letter)
+
+    newpos = (pos-secret)
+
+    if newpos >= 29:
+        newpos = newpos - 29
+    return alphabet[newpos]
+
+def decode(letter, secret):
+    pos = alphabet.find(letter)
+    newpos = (pos-int(secret))
+
+    if newpos < 0:
+        newpos = newpos + 29 
+    return alphabet[newpos]
+
+def main():
+    command = input("Decode or encode?: ")
+    secret = input("What's the secret number?: ")
+    if(command=="decode"):
+        message = input("The message you want to decode: ")
+        output = ""
+
+        for character in message:
+            if character in alphabet:
+                output = output + decode(character, secret)
+            else:
+                output = output + character
+    if(command=="encode"):
+        message = input("The message you want to encode: ")
+        output = ""
+
+        for character in message:
+            if character in alphabet:
+                output = output + encode(character, secret)
+            else:
+                output = output + character
+
+
+    print(output)
+
+main()
