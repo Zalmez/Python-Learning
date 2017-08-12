8fd75cc940605f7ddd44877c925868ea99a70274
Cato Myhre <boredgamerz1998@gmail.com>
2017-08-12 19:19:10 +0200

    added windows 10 syntheziser
    added a windows 10 compatable syntheziser. this will not work with other oses as far as I know.

--- User Input & While Loops/parrot.py
+++ User Input & While Loops/parrot.py
@@ -1,2 +1,16 @@
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+import win32com.client as wincl
+import logging
+
+def synth(strVal):
+    if strVal is None:
+        logging.debug("You have not set a value. Set a value before using synth")
+    else:
+        speak = wincl.Dispatch("SAPI.spVoice")
+        speak.Speak(strVal)
+
+def main():
+    message = input("Tell me something, and I will repeat it back to you: ")
+    synth(message)
+
+main()
+
